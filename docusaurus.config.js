1b683b5fccd474d8396e7b65b08c0141c109aed7
DalexHD <eldalexhd@gmail.com>
2021-12-26 20:31:08 +0000

    feat: update url.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 module.exports = {
   title: "My Personal Website",
   tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://dalexhd.dev",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
